de3428473eceed8d3a01bb80bd272c10ffd35aa7
Kir Proto Q <kir.proto@ya.ru>
2022-02-09 11:42:08 +0500

    Initial Commit

---
+++ Yan_SwiftUI_Zhesty/Yan_SwiftUI_ZhestyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_ZhestyApp.swift
+//  Yan_SwiftUI_Zhesty
+//
+//  Created by KIR Q on 2022.02.09.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_ZhestyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
